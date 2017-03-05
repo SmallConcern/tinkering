389d3af2c6f5aa4d2c0bc6d2b0fe23ec038ac7ff
casey small <casey small>
2017-03-05 13:44:55 -0600

    Fixed assertions in test_rotation_sot.

--- rotated_sort/test_rotation_sort.py
+++ rotated_sort/test_rotation_sort.py
@@ -57,18 +57,18 @@ class TestFindTargetFromRotatedSorted:
         assert "'10' is not in input list." in str(exc.value)
 
     def test_get_target_index_in_non_rotated_list(self):
-        SortedRotationSearcher.find_target([1], 1) == 0
-        SortedRotationSearcher.find_target([1, 2], 2) == 1
-        SortedRotationSearcher.find_target([1, 2], 1) == 0
-        SortedRotationSearcher.find_target([1, 2, 3, 4, 5, 6, 7, 8], 5) == 4
-        SortedRotationSearcher.find_target([1, 2, 3, 4, 5, 6, 7, 8], 1) == 0
-        SortedRotationSearcher.find_target([1, 2, 3, 4, 5, 6, 7, 100], 100) == 7
+        assert SortedRotationSearcher.find_target([1], 1) == 0
+        assert SortedRotationSearcher.find_target([1, 2], 2) == 1
+        assert SortedRotationSearcher.find_target([1, 2], 1) == 0
+        assert SortedRotationSearcher.find_target([1, 2, 3, 4, 5, 6, 7, 8], 5) == 4
+        assert SortedRotationSearcher.find_target([1, 2, 3, 4, 5, 6, 7, 8], 1) == 0
+        assert SortedRotationSearcher.find_target([1, 2, 3, 4, 5, 6, 7, 100], 100) == 7
 
     def test_get_target_index_in_rotated_list(self):
-        SortedRotationSearcher.find_target([2, 1], 1) == 1
-        SortedRotationSearcher.find_target([2, 1], 2) == 0
-        SortedRotationSearcher.find_target([9, 10, 11, 1, 6, 8], 8) == 5
-        SortedRotationSearcher.find_target([9, 10, 11, 1, 6, 8], 9) == 0
-        SortedRotationSearcher.find_target([9, 10, 11, 1, 6, 8], 11) == 2
-        SortedRotationSearcher.find_target([25, 1, 2, 3, 4], 25) == 0
-        SortedRotationSearcher.find_target([25, 100, 200, 201, 4], 4) == 4
+        assert SortedRotationSearcher.find_target([2, 1], 1) == 1
+        assert SortedRotationSearcher.find_target([2, 1], 2) == 0
+        assert SortedRotationSearcher.find_target([9, 10, 11, 1, 6, 8], 8) == 5
+        assert SortedRotationSearcher.find_target([9, 10, 11, 1, 6, 8], 9) == 0
+        assert SortedRotationSearcher.find_target([9, 10, 11, 1, 6, 8], 11) == 2
+        assert SortedRotationSearcher.find_target([25, 1, 2, 3, 4], 25) == 0
+        assert SortedRotationSearcher.find_target([25, 100, 200, 201, 4], 4) == 4
